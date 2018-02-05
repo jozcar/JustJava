58960ed29fafeaf4047b4f94a758366a9cd3984c
jozcar <jozcar@yahoo.com>
2018-02-04 21:22:00 -0500

    updated up

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 
 
+import org.w3c.dom.Text;
+
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,19 +22,19 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view){
 
         display(order.currentCoffeeCount());
-        displayPrice(order.currentCoffeeCount() * 3);
+        displayMessage();
     }
 
     public void increaseOrder(View view){
         order.add();
         display(order.currentCoffeeCount());
-        displayPrice(order.currentCoffeeCount() * 3);
+        displayMessage();
 
     }
     public void decreaseOrder(View view){
         order.subtract();
         display(order.currentCoffeeCount());
-        displayPrice(order.currentCoffeeCount() * 3);
+        displayMessage();
     }
 
 
@@ -40,8 +42,14 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    private void displayPrice(int number){
+    private  void displayMessage(){
+        String amount_final_msg = String.format("Amount due:$%s",order.currentCoffeeCount() * 3);
+        amount_final_msg = amount_final_msg + "\nThank You.";
         TextView priceTextView = findViewById(R.id.quantity_price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        priceTextView.setText(amount_final_msg);
     }
+    //private void displayPrice(int number){
+      //  TextView priceTextView = findViewById(R.id.quantity_price_text_view);
+        //priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    //}
 }
